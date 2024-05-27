bbd1cfac48cc8a6e3259d20a1ccf2982e71fe2a2
callsmasterofficial <partnerchc@outlook.com>
2024-05-27 14:38:34 +0530

    shxz

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 
-export default {
+export {
     Footer,
-    Header
+    Header,
+    Container,
+    Logo,
+    LogoutBtn
 }
